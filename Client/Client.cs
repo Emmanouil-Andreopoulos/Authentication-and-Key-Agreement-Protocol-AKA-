af4d1b3b5bef6672741a54e6b8a2ebe97ec01282
Manolis Andreopoulos <manolisandreopoulos@gmail.com>
2017-10-13 20:17:43 +0300

    Added support for SHA256 in client and server

--- Client/Client.cs
+++ Client/Client.cs
@@ -151,5 +151,44 @@ namespace Client
                 return false;
             }
         }
+
+        static string GetSHA256Hash(SHA256 mySHA256, string input)
+        {
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = mySHA256.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+
+        // Verify a hash against a string.
+        static bool VerifySHA256Hash(SHA256 mySHA256, string input, string hash)
+        {
+            // Hash the input.
+            string hashOfInput = GetSHA256Hash(mySHA256, input);
+
+            // Create a StringComparer an compare the hashes.
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            if (0 == comparer.Compare(hashOfInput, hash))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
