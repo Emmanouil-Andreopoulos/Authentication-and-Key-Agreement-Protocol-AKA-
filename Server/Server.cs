b97c7aaa39ed0334269cadbca0694da447f25b1a
Manolis Andreopoulos <manolisandreopoulos@gmail.com>
2017-10-20 19:01:53 +0300

    Add HMACSHA256 to server

--- Server/Server.cs
+++ Server/Server.cs
@@ -311,5 +311,44 @@ namespace Server
 
             return plaintext;
         }
+
+        static string GetHMACSHA256Hash(HMACSHA256 myHMACSHA256, string input)
+        {
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = myHMACSHA256.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+
+        // Verify a hash against a hash.
+        static bool VerifyHMACSHA256Hash(HMACSHA256 myHMACSHA256, string input, string hash)
+        {
+            // Hash the input.
+            string hashOfInput = GetHMACSHA256Hash(myHMACSHA256, input);
+
+            // Create a StringComparer an compare the hashes.
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            if (0 == comparer.Compare(hashOfInput, hash))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
